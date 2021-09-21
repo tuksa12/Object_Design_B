36779304c52beb19a88f2f44df89839afc95439e
Christian Femers <femers@in.tum.de>
2021-09-22 00:52:29 +0200

    Require students to implement this

--- src/de/tum/in/ase/eist/TwoFactorAuthentication.java
+++ src/de/tum/in/ase/eist/TwoFactorAuthentication.java
@@ -2,16 +2,8 @@ package de.tum.in.ase.eist;
 
 public class TwoFactorAuthentication {
 
-    private PasswordAuthentication passwordAuthentication = new PasswordAuthentication();
-
     public boolean authenticate(String loginName, LoginRequest request) {
-        boolean passwordCorrect = passwordAuthentication.authenticate(loginName, request);
-        if (!passwordCorrect) {
-            return false;
-        }
-        System.out.println("Sending second factor request...");
-        System.out.println(">> Success!");
-        return true;
+        return false; // TODO
     }
 
 }
